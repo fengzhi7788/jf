05c34fedd7932d336033a26361bfbfa7fc4332a1
fengzhi7788 <fzfz@admindeiMac-62.local>
2016-04-18 20:47:19 +0800

    1
    1

---
+++ jf/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  jf
+//
+//  Created by fz on 16/4/18.
+//  Copyright © 2016年 fz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBOutlet weak var xianshi: UITextField!
+
+    @IBAction func add(sender: UIButton) {
+    }
+
+    @IBAction func sub(sender: UIButton) {
+    }
+}
+
